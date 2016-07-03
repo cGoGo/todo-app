3f8858cad6de5fc39dbe25661953bfad3f344a52
Camilina <castegong@gmail.com>
2016-07-03 18:36:41 -0400

    Avances en ejercicio

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+
+//Pasos a seguir:
+//1.- Creamos el elemento (createElement)
 var contTarea = document.createElement("li");
-var textTarea = document.getElementById("tareaIngresada").value;
+
+//2.- Creamos el contenido, nodo de texto (createTextNode)
+var texto = document.getElementById("tareaIngresada");
+var textoTarea = document.createTextNode(texto.value);
+
+//3.- Añadimos el nodo de texto al elemnto que creamos (appendChild)
+contTarea.appendChild(textoTarea);
+
+//4.- Agregamos el elemento al DOM (llamamos al padre por el Id y agre-
+//		gamos el elemento)
+document.getElementById("contenedor-tareas").appendChild(contTarea);
