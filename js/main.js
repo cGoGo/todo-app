ed996c3989b96e51390c29af324161a6c39e16c9
Camilina <castegong@gmail.com>
2016-07-04 12:23:24 -0400

    Checkbox tacha texto al hacer click

--- js/main.js
+++ js/main.js
@@ -11,15 +11,17 @@ botonAgregar.onclick = function () {								//al dar click al boton
 	} else {
 		var tareaIngresada = inputTarea.value, 						//valor de input
 			nuevaTarea = document.createElement("li"),			//creamos li para añadir
+			contTexto = document.createElement("span"),			//creamos un span para contener el texto dentro del li
 			textoTarea = document.createTextNode(tareaIngresada), //creamos un nodo de texto con la info del input
 			basura = document.createElement("i"),							//creamos un i para crear el basurero
 			check = document.createElement("input");							//creamos checkbox
 
 		check.setAttribute("type", "checkbox");									//agregamos atributos el checkbox
-		check.setAttribute("class", "checkbox");
+		check.setAttribute("class", "checkbox");								//agregamos atributos el checkbox
 		nuevaTarea.appendChild(check);													//agregamos checkbox a li
 		nuevaTarea.setAttribute("class", "contenedor-tareas"); //agregamos una clase al li
-		nuevaTarea.appendChild(textoTarea);								//agregamos el texto al li
+		contTexto.appendChild(textoTarea);								//agregamos el texto al span
+		nuevaTarea.appendChild(contTexto);								//agregamos el span al li
 		contenedorTareas.appendChild(nuevaTarea);					//agregamos el li a la lista
 		basura.setAttribute("class", "fa fa-trash-o icono-borrar"); //agregamos icono
 		basura.setAttribute("id", "basura");							//agregamos un id al basurero para mas delante
@@ -29,10 +31,14 @@ botonAgregar.onclick = function () {								//al dar click al boton
 		basura.onclick = function () {					//para borrar al dar click al icono
 			contenedorTareas.removeChild(nuevaTarea);
 		};
-		//check.onchange = function () {
-			//nuevaTarea.textContent.strike();  // no puedo hacer funcionar el checkbox (aun)
-		//};
-	}
+		check.onclick = function () {
+			if (this.checked == true) {
+				contTexto.setAttribute("class", "tachado");
+			} else {
+				contTexto.removeAttribute("class");
+			}
+		};
+	};
 };
 
 
